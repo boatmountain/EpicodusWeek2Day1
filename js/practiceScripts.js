0b08f38913be9b700c7168fbd5e1eeda41cf1673
Dominic Montelongo <padma@email.com>
2020-04-02 04:23:45 -0700

    Add regular expressions examples from text

--- js/practiceScripts.js
+++ js/practiceScripts.js
@@ -12,3 +12,14 @@ saySomething(outputText);
 
 // Refactored 3 lines into 1
 saySomething("The sum is " + add(3, 4) + ".");
+
+// To use regular expressions to find and replace text that matches patterns, try using the built-in JavaScript method, replace(). 
+
+var str = 'Twas the night before Xmas...';
+var newstr = str.replace(/xmas/i, 'Christmas');
+alert(newstr);  
+
+var re = /apples/gi;
+var str = 'Apples are round, and apples are juicy.';
+var newstr = str.replace(re, 'oranges');
+console.log(newstr);  
